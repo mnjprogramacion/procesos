b7bea2204ed61aefd21e159322bf230d720724a9
matt <mnjprogramacion@gmail.com>
2025-09-17 21:40:46 +0200

    Update App local

--- Project/src/App.java
+++ Project/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("I don't care.");
     }
 }
