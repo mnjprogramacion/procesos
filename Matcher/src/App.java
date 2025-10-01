ea07ec8c68e05a597484645b48c5c6c792e822d9
mnjprogramacion <mnjprogramacion@gmail.com>
2025-10-01 18:03:00 +0200

    Proyecto Matcher creado, texto importado

---
+++ Matcher/src/App.java
@@ -0,0 +1,28 @@
+import java.util.regex.Matcher;
+
+public class App {
+    public static void main(String[] args) {
+
+        Matcher match;
+
+        String text = "\n{Un hombre} va al médico porque tiene un problema muy grave: cada vez que estornuda, {} se convierte en un animal diferente. {El médico} le dice que es un caso muy raro y que necesita hacerle unas pruebas. {Le} pone unos electrodos en la cabeza y le conecta a una máquina que mide su actividad cerebral." + //
+                        "\n\t-Bien, ahora voy a provocarle un estornudo con este spray nasal y veremos qué pasa - dice {el médico}." +
+                        "\n\t-Vale, pero tenga cuidado, no sé en qué me voy a convertir - dice {el hombre}." +
+                        "\n\t-No se preocupe, tengo una jaula preparada por si acaso - dice {el médico}." +
+                        "\n{El médico} le aplica el spray nasal al hombre y este empieza a estornudar. De repente, se transforma en un león y empieza a rugir y a intentar escapar de la jaula." +
+                        "\n\t-¡Increíble! - exclama {el médico} - Se ha convertido en un león. Esto es fascinante. Voy a anotar los resultados." +
+                        "\n{El médico} le aplica otro spray nasal al hombre y este vuelve a estornudar. Esta vez, se transforma en un elefante y empieza a trompetear y a mover la trompa." +
+                        "\n\t-¡Increíble! - repite {el médico} - Ahora es un elefante. Esto es asombroso. Voy a anotar los resultados." +
+                        "\n\t-{El médico} le aplica otro spray nasal al hombre y este estornuda de nuevo. Ahora, se transforma en un pingüino y empieza a graznar y a deslizarse por el suelo." +
+                        "\n\t-¡Increíble! - dice {el médico} por tercera vez - Ahora es un pingüino. Esto es extraordinario. Voy a anotar los resultados." +
+                        "\n{El médico} le aplica otro spray nasal al hombre y este estornuda una vez más. Pero esta vez, no se transforma en ningún animal, sino que se queda como estaba." +
+                        "\n\t-¡Increíble! - dice {el médico} por última vez - Ahora ha vuelto a la normalidad. Esto es milagroso. Voy a anotar los resultados." +
+                        "\nEl médico le quita los electrodos al hombre y le dice:" +
+                        "\n\t-Bueno, señor, he terminado las pruebas. Ya sé cuál es su problema." +
+                        "\n\t-¿De verdad? ¿Y qué es? - pregunta el hombre, esperanzado." +
+                        "\n\t-Tiene usted una alergia muy severa al spray nasal.";
+
+        System.out.printf(text);
+
+    }
+}
