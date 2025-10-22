b4db8a6c92d2b7e56b3587dbcdc2e35c7b816010
mnjprogramacion <mnjprogramacion@gmail.com>
2025-10-22 18:58:55 +0200

    UPD interfaz login (cambio a grid layout)

--- interfaz-login/src/login/verde/newpackage/ui/Login.java
+++ interfaz-login/src/login/verde/newpackage/ui/Login.java
@@ -16,21 +16,22 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jTextFieldUrl = new javax.swing.JTextField();
-        jPasswordContr = new javax.swing.JPasswordField();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jTextFieldCorreo = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
+        jTextFieldUrl = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
         jTextFieldUsuario = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jPasswordContr = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
         setMaximumSize(new java.awt.Dimension(500, 500));
         setMinimumSize(new java.awt.Dimension(500, 500));
-        setPreferredSize(new java.awt.Dimension(500, 500));
         setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
@@ -38,19 +39,35 @@ public class Login extends javax.swing.JFrame {
         jPanel1.setMinimumSize(new java.awt.Dimension(500, 500));
         jPanel1.setName(""); // NOI18N
         jPanel1.setPreferredSize(new java.awt.Dimension(500, 500));
-        jPanel1.setLayout(null);
+        jPanel1.setLayout(new java.awt.GridLayout(12, 1));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 102, 102));
         jLabel1.setText("LOGIN");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(180, 10, 110, 48);
 
-        jLabel2.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel2.setText("Usuario");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(80, 250, 50, 20);
+        jLabel5.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPanel1.add(jLabel5);
+
+        jLabel8.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel8.setText("Correo electrónico");
+        jPanel1.add(jLabel8);
+
+        jTextFieldCorreo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jTextFieldCorreo.setForeground(new java.awt.Color(102, 102, 102));
+        jTextFieldCorreo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldCorreoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextFieldCorreo);
+
+        jLabel9.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel9.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel9.setText("URL");
+        jPanel1.add(jLabel9);
 
         jTextFieldUrl.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jTextFieldUrl.setForeground(new java.awt.Color(102, 102, 102));
@@ -60,7 +77,25 @@ public class Login extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jTextFieldUrl);
-        jTextFieldUrl.setBounds(80, 190, 324, 38);
+
+        jLabel2.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("Usuario");
+        jPanel1.add(jLabel2);
+
+        jTextFieldUsuario.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jTextFieldUsuario.setForeground(new java.awt.Color(102, 102, 102));
+        jTextFieldUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextFieldUsuario);
+
+        jLabel3.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setText("Contraseña");
+        jPanel1.add(jLabel3);
 
         jPasswordContr.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -68,13 +103,10 @@ public class Login extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jPasswordContr);
-        jPasswordContr.setBounds(80, 370, 324, 38);
 
-        jLabel3.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel3.setText("Contraseña");
-        jPanel1.add(jLabel3);
-        jLabel3.setBounds(80, 340, 80, 20);
+        jLabel4.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPanel1.add(jLabel4);
 
         jButton1.setBackground(new java.awt.Color(0, 102, 102));
         jButton1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
@@ -86,39 +118,6 @@ public class Login extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(80, 440, 100, 36);
-
-        jLabel8.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel8.setText("Correo electrónico");
-        jPanel1.add(jLabel8);
-        jLabel8.setBounds(80, 70, 120, 20);
-
-        jTextFieldCorreo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jTextFieldCorreo.setForeground(new java.awt.Color(102, 102, 102));
-        jTextFieldCorreo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldCorreoActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextFieldCorreo);
-        jTextFieldCorreo.setBounds(80, 100, 324, 38);
-
-        jLabel9.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel9.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel9.setText("URL");
-        jPanel1.add(jLabel9);
-        jLabel9.setBounds(80, 160, 25, 20);
-
-        jTextFieldUsuario.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jTextFieldUsuario.setForeground(new java.awt.Color(102, 102, 102));
-        jTextFieldUsuario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldUsuarioActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextFieldUsuario);
-        jTextFieldUsuario.setBounds(80, 280, 324, 38);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -201,6 +200,8 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
