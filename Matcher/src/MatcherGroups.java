b89a196fb6d26c15144590e6ec0c7a0f543d1342
mnjprogramacion <mnjprogramacion@gmail.com>
2025-10-29 21:41:38 +0100

    ADD MatcherGroups

---
+++ Matcher/src/MatcherGroups.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatcherGroups {
+    public static void main(String[] args) {
+
+        String text = "\nEl hombre dijo 'El perro no sabe leer' y el perro le contestó 'Si me hubieras preguntado sabrías la verdad'. 'Cuál es la verdad' preguntó el hombre. 'La verdad es ...' y el perro sonó como 'aaaaagh'.";
+
+        String regex = "'([^']*)'";
+        Pattern p1 = Pattern.compile(regex);
+        Matcher m1 = p1.matcher(text);
+        m1.find();
+
+        System.out.println(m1.group());
+    }
+}
+
+
+
+"(.+?)"
