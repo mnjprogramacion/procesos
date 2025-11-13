9155f8a6b5f9f271ceddc7f6a1d8c90b66a89525
mnjprogramacion <mnjprogramacion@gmail.com>
2025-11-13 17:00:33 +0100

    UPD multiplicacion

--- Matcher/src/RecursividadMultiplicacion.java
+++ Matcher/src/RecursividadMultiplicacion.java
@@ -1,3 +1,14 @@
 public class RecursividadMultiplicacion {
-    
+ 
+    public static void multiplicar(int n1, int n2) {
+        if ((n1 == 0) || (n2 == 0)) {
+            System.out.printf("\nResultado: %d", n1);
+        } else {
+            System.out.printf("");
+        }
+    }
+ 
+    public static void main(String[] args) {
+        multiplicar(0, 0); // Comienza la recursión
+    }
 }
