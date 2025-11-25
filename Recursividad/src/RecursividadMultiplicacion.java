030b66ea0fbdcbfbff9f7af30d370511c4232540
mnjprogramacion <mnjprogramacion@gmail.com>
2025-11-25 17:40:57 +0100

    UPD RecursividadMultiplicacion

--- Recursividad/src/RecursividadMultiplicacion.java
+++ Recursividad/src/RecursividadMultiplicacion.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class RecursividadMultiplicacion {
  
     public static int multiplicar(int n1, int n2) {
 
-        if (n2 == 0) {
-            return n2;
+        if ((n1 == 0) || (n2 == 0)) {
+            return 0;
         }
 
         if (n2 == 1) {
@@ -16,7 +18,15 @@ public class RecursividadMultiplicacion {
  
     public static void main(String[] args) {
 
-        System.out.printf("\nResultado: %d", multiplicar(2, 2));
+        Scanner entrada = new Scanner(System.in);
+        int n1, n2;
+
+        System.out.printf("\nIntroduce el primer número: ");
+        n1 = entrada.nextInt();
+        System.out.printf("Introduce el segundo número: ");
+        n2 = entrada.nextInt();
+
+        System.out.printf("Resultado: %d", multiplicar(n1, n2));
 
     }
 }
