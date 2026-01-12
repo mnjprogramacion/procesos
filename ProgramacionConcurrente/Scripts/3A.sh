e478ed8d5121886faadc48c37732639d00a35a4f
mnjprogramacion <mnjprogramacion@gmail.com>
2026-01-12 18:34:35 +0100

    UPD 3A 3B

---
+++ ProgramacionConcurrente/Scripts/3A.sh
@@ -0,0 +1,7 @@
+echo "Dime tus tres aficiones favoritas:"
+read -p "1. " af1
+read -p "2. " af2
+read -p "3. " af3
+read -p "¿Por qué te gusta $af2? " razones
+export razones
+./3B.sh
---
+++ ProgramacionConcurrente/Scripts/3B.sh
@@ -0,0 +1,1 @@
+echo $razones > razones.txt
