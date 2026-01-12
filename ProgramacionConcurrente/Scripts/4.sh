8a3008864b5f9a0a6124213e5d4440f1f5227d01
mnjprogramacion <mnjprogramacion@gmail.com>
2026-01-12 18:54:17 +0100

    ADD 4.sh

---
+++ ProgramacionConcurrente/Scripts/4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HP_JUGADOR=100
+HP_BOSS=100
+HEROE=${1:-"Aventurero"}
+JEFE="El Rey del Bug"
+
+# selección de clase
+clear
+echo "=== BASH LEGENDS II: EL REY DEL BUG ==="
+echo ""
+echo "Bienvenido, $HEROE. ¿Cuál es tu clase?"
+echo "1) Mago"
+echo "2) Paladín"
+echo "3) Arquero"
+echo "4) Bardo"
+read -p "Elige un número (1-4): " CLASE_NUM
+
+case $CLASE_NUM in
+    1|*) CLASE="Mago";;
+    2) CLASE="Paladín";;
+    3) CLASE="Arquero";;
+    4) CLASE="Bardo";;
+esac
+
+echo "¡Has elegido ser un $CLASE!"
